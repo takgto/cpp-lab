92dfe439594d3259f01080b87903b85472d61a2c
TGoto <takgoto@g.ecc.u-tokyo.ac.jp>
2026-08-19 19:48:35 +0900

    Add src files.

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "util.h"
+int main() {
+    std::cout << add(3, 4) << std::endl;
+    return 0;
+}
---
+++ src/util.cpp
@@ -0,0 +1,2 @@
+#include "util.h"
+int add(int a, int b) { return a + b; }
